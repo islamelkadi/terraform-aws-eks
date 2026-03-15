2735ca8e618b06d94d6d787c9345e04302c7884e
Islam Elkadi <153553770+islamelkadi@users.noreply.github.com>
2026-03-14 23:45:08 -0400

    Update metadata.tf

--- metadata.tf
+++ metadata.tf
@@ -2,11 +2,11 @@
 # Provides standardized naming, tagging, and security controls
 
 module "metadata" {
-  source = "github.com/islamelkadi/terraform-aws-metadata?ref=v1.0.3"
+  source = "github.com/islamelkadi/terraform-aws-metadata?ref=v1.1.0"
 
   namespace     = var.namespace
   project_name  = var.name
   environment   = var.environment
-  resource_type = "cloudfront"
+  resource_type = "eks"
   region        = var.region
 }
